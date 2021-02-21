d8c7de6068e1861319c8b3cf8da193aee357a7cc
Bishal Bashyal <63231700+bishalbashyal33@users.noreply.github.com>
2021-02-21 12:40:55 +0545

    rk methods

---
+++ eulersmethod.cpp
@@ -0,0 +1,59 @@
+/*
+Start
+
+2. Define function f(x,y)
+
+3. Read values of initial condition(x0 and y0), 
+   number of steps (n) and calculation point (xn)
+   
+4. Calculate step size (h) = (xn - x0)/n
+
+5. Set i=0
+
+6. Loop 
+      
+      yn = y0 + h *  f(x0 + i*h, y0)
+      
+      y0 = yn
+      
+      i = i + 1
+
+   While i < n
+
+7. Display yn as result
+
+8. Stop
+
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+float f(float x,float y);
+int main()
+{
+    float x0,y0,n,xn,h,yn;
+    cout<<"Enter intial values, num of steps and calculation poin";
+    cin>>x0>>y0>>n>>xn;
+
+    float sh = (xn-x0)/n;
+   
+
+    int i =0;
+
+    do{
+        yn =y0+h*(f(x0+i*h,y0));
+        y0=yn;
+        i++;
+        
+
+    }while(i<n);
+
+    
+    cout<<"solution is"<<yn;
+    return 0;
+}
+
+float f(float x,float y){
+    return pow(x,2)-cos(x)+pow(y,2);
+}
---
+++ rk2method.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+float dyx(float x, float y){
+    return x+y-2;
+}
+
+float rk(float x0,float y0, float x,float h){
+    int n = int((x-x0)/h);
+    float k1,k2;
+    float y =y0;
+    for(int i =0;i<n;i++){
+         k1 = h * dyx(x0, y); 
+          k2 = h * dyx(x0 + 0.5 * h, 
+                      y + 0.5 * k1); 
+        y = y + (1.0 / 6.0) * (k1 + 2 * k2); 
+        x0 = x0 + h; 
+
+    }
+    return y;
+}
+
+int main(){
+    float x0 =0,y=5,x=2,h=0.01;
+    cout<<"Solution is:"<<rk(x0,y,x,h);
+    return 0;
+}
