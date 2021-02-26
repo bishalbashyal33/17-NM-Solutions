2aea512e3e91e5750f9086d1b7840d925fcd4a8d
Bishal Bashyal <63231700+bishalbashyal33@users.noreply.github.com>
2021-02-26 22:26:19 +0545

    minor changes

--- rk4.cpp
+++ rk4.cpp
@@ -56,7 +56,7 @@ int main(){
         float k2= h*f(x0+h/2,y0+k1/2);
         float k3 = h*f(x0+h/2,y0+k2/2);
         float k4 = h*f(x0+h,y0+k3);
-        float k=(k1+2*k2+3*k3+k4)/6;
+        float k=(k1+2*k2+2*k3+k4)/6;
         yn = y0+k;
         i++;
         x0+=h;
